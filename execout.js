6a3196b048439dd8f13386c6daf58f497b8610d1
Thanh Kieu <thanhpk@live.com>
2016-08-09 15:38:54 +0100

    reformat

--- execout.js
+++ execout.js
@@ -1,40 +1,40 @@
 (function(){
 	"use strict";
-var spawn = require('child_process').spawn;
-var path = require('path');
+	var spawn = require('child_process').spawn;
+	var path = require('path');
 
-exports.execout = function (command, cb)
-{
-	// convert relative path comand to absoute path comamnd
-	// since spawn function dont allow relative path
-	if(isRelativePath(command)){
-		command = path.join(path.resolve('./'), command);
+	exports.execout = function (command, cb)
+	{
+		// convert relative path comand to absoute path comamnd
+		// since spawn function dont allow relative path
+		if(isRelativePath(command)){
+			command = path.join(path.resolve('./'), command);
+		}
+
+		var cmd = spawn(command, {std: 'inherit', shell:true});
+
+		var output = "";
+		
+		cmd.stdout.on('data', function(data){
+			output+=data;
+			process.stdout.write(data.toString());
+		});
+
+		var isError = undefined;
+		cmd.stderr.on('data', function(data){
+			isError = true;
+			output+=data;
+			process.stdout.write('ERR: ' + data);
+		});
+
+		cmd.on('exit', function(code){
+			if(cb) cb(isError, output, code);
+		});
 	}
 
-	var cmd = spawn(command, {std: 'inherit', shell:true});
-
-	var output = "";
-	
-	cmd.stdout.on('data', function(data){
-		output+=data;
-		process.stdout.write(data.toString());
-	});
-
-	var isError = undefined;
-	cmd.stderr.on('data', function(data){
-		isError = true;
-		output+=data;
-		process.stdout.write('ERR: ' + data);
-	});
-
-	cmd.on('exit', function(code){
-		if(cb) cb(isError, output, code);
-	});
-}
-
-function isRelativePath(path)
-{
-	return path.startsWith('./') || path.startsWith('../');
-}
+	function isRelativePath(path)
+	{
+		return path.startsWith('./') || path.startsWith('../');
+	}
 
 })();
